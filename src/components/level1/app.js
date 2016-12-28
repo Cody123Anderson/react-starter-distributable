4e59d64992cc32b92985deac45fe6ca62b47144e
Cody Anderson <cody.anderson@insidesales.com>
2016-12-28 09:27:09 -0700

    hello world to be cloned

--- src/components/level1/app.js
+++ src/components/level1/app.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import LandingPage from '../level2/landing-page';
+import HomePage from '../level2/home-page';
 
 const mountNode = document.createElement('div');
 
 document.body.appendChild(mountNode);
-ReactDOM.render(<LandingPage />, mountNode);
+ReactDOM.render(<HomePage />, mountNode);
